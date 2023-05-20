2e1c32793dc62d254d3e618f6b6ed645220c4d59
misikira <misikirteka@gmail.com>
2023-05-20 18:35:51 +0300

    add fade to meu

--- starter/script.js
+++ starter/script.js
@@ -120,5 +120,34 @@ tabcontnainer.addEventListener('click',function (e) {
  //add  active class
   document.querySelector(`.operations__content--${clicked.dataset.tab}`)
   .classList.add('operations__content--active')
-}
-)
+});
+
+//menu fade navigation
+const nav=document.querySelector('.nav')
+nav.addEventListener('mouseover',function (e) {
+  if(e.target.classList.contains('nav__link')){
+    const link=e.target;
+    const siblings=link.closest('.nav').querySelectorAll('.nav__link');
+    const logo=link.closest('.nav').querySelector('img');
+  
+siblings.forEach(el=>{
+  if(el !== link) el.style.opacity=0.5;
+  });
+logo.style.opacity=0.5;
+  }
+});
+ nav.addEventListener('mouseout',function (e) 
+  
+  
+ {
+  if(e.target.classList.contains('nav__link')){
+    const link=e.target;
+    const siblings=link.closest('.nav').querySelectorAll('.nav__link');
+    const logo=link.closest('.nav').querySelector('img');
+  
+siblings.forEach(el=>{
+  if(el !== link) el.style.opacity=1;
+  });
+logo.style.opacity=1;
+  }
+});
