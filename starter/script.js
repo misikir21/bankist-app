d67edd8c090248b7d510f44726d732fa469e8bc1
misikira <misikirteka@gmail.com>
2023-05-22 16:22:35 +0300

    add lazy load images

--- starter/script.js
+++ starter/script.js
@@ -171,20 +171,50 @@ const headerobserver=new IntersectionObserver(sitckyNav,{
 headerobserver.observe(stickyheader)
 
 //reaval section on scroll
-const allsections=document.querySelectorAll('.section')
-allsections.forEach(function (section) {
-  sectionobserver.observe('section');
-  section.classList.add('section--hidden')
-  
-})
-const revalsection=function (entries,observe) {
-  const [entry]=entries;
-  if(!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden')
-}
-const sectionobserver=new IntersectionObserver(
-  revalsection,{
-  root:null,
-  threshold:0.15
-  }
-)
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
